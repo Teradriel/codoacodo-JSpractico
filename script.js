4f63ae4039d2a9e59732082619ee57142dc18bfe
Luca <luca.terzariol@outlook.com>
2022-05-29 12:56:29 +0200

    entrega Trabajo práctico integrador JS

---
+++ script.js
@@ -0,0 +1,19 @@
+var cantidad = 0;
+var total = 0;
+var categoria = "";
+
+function totalPago() {
+  cantidad = document.getElementById("cantidad").value;
+  categoria = document.getElementById("categoria").value;
+  var total = cantidad * 200;
+  if (categoria == "") {
+    alert("Seleccione una categoria");
+  } else if (categoria == "Trainee") {
+    total = total * 0.5;
+  } else if (categoria == "Junior") {
+    total = total * 0.85;
+  } else if (categoria == "Estudiante") {
+    total = total * 0.2;
+  }
+  document.getElementById("total").innerHTML = "Total a Pagar: $" + total;
+}
